a61dc06b44ac451791f69412b99a694718104463
unknown <mahekmj123@gmail.com>
2023-03-26 21:37:45 +0530

    home screen completed

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,106 @@
+import 'package:booktickets/screens/hotel_screen.dart';
+import 'package:booktickets/screens/ticket_view.dart';
+import 'package:booktickets/utils/app_info_list.dart';
+import 'package:booktickets/utils/app_styles.dart';
+import 'package:booktickets/widgets/double_text_widget.dart';
+import 'package:fluentui_icons/fluentui_icons.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Styles.bgColor,
+      body: ListView(
+        children: [
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              children: [
+                const Gap(40),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                            "Good morning!", style: Styles.headLineStyle3,
+                        ),
+                        const Gap(5),
+                        Text(
+                            "Book Ticket", style: Styles.headLineStyle1,
+                        ),
+                      ],
+                    ),
+
+                    Container(
+                      height: 50,
+                      width: 50,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        image: const DecorationImage(
+                          fit: BoxFit.fitHeight,
+                          image: AssetImage(
+                            "assets/images/logo.png"
+                          )
+                        )
+                      ),
+                    )
+                  ],
+                ),
+                const Gap(25),
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: const Color(0xFFF4F6FD),
+                  ),
+                  padding: EdgeInsets.symmetric(horizontal: 12, vertical: 12),
+                  child: Row(
+                    children: [
+                      const Icon(FluentSystemIcons.ic_fluent_search_regular, color: Color(0xFFBFC205),),
+                      Text(
+                          "Search",
+                          style: Styles.headLineStyle4,
+                      )
+                    ],
+                  ),
+                ),
+                const Gap(40),
+                const AppDoubleTextWidget(bigText: "Upcoming Flights", smallText: "View All",),
+              ],
+            ),
+          ),
+          const Gap(15),
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            padding: const EdgeInsets.only(left:20),
+            child: Row(
+              children: const [
+                TicketView(),
+                TicketView(),
+              ],
+            ),
+          ),
+          const Gap(15),
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: const AppDoubleTextWidget(bigText: "Hotels", smallText: "View All",),
+          ),
+          const Gap(15),
+          SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              padding: const EdgeInsets.only(left:20),
+            child: Row(
+              children: hotelList.map((singleHotel) => HotelScreen(hotel: singleHotel)).toList()
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/utils/app_info_list.dart
@@ -0,0 +1,53 @@
+List<Map<String, dynamic>> hotelList = [
+  {
+    'image': 'one.jpg',
+    'place': 'Open Space',
+    'destination' : 'London',
+    'price': 35
+  },
+  {
+    'image': 'two.jpg',
+    'place': 'Global Will',
+    'destination' : 'London',
+    'price': 25
+  },
+  {
+    'image': 'three.jpg',
+    'place': 'Tallest Building',
+    'destination' : 'Dubai',
+    'price': 40
+  },
+  {
+    'image': 'four.jpg',
+    'place': 'India Gate',
+    'destination' : 'India',
+    'price': 68
+  },
+];
+
+List<Map<String, dynamic>> ticketList = [
+  {
+    'image': 'one.png',
+    'place': 'Open Space',
+    'destination' : 'London',
+    'price': 35
+  },
+  {
+    'image': 'two.png',
+    'place': 'Global Will',
+    'destination' : 'London',
+    'price': 25
+  },
+  {
+    'image': 'three.png',
+    'place': 'Tallest Building',
+    'destination' : 'Dubai',
+    'price': 40
+  },
+  {
+    'image': 'four.png',
+    'place': 'India Gate',
+    'destination' : 'India',
+    'price': 68
+  },
+];
